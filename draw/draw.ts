1de65b79d33acc3d79b749716b2c6d19d20a5aea
huddj <josh.huddleston@icloud.com>
2022-05-25 14:06:37 -0400

    Remove refresh after fromJSON

--- draw/draw.ts
+++ draw/draw.ts
@@ -661,7 +661,6 @@ class UserInterface {
                 return new Shape(basic.name, basic.origin, polygons, basic.shapes.map(s => convertObjToShape(s)));
             }
             Game.GAME.model = convertObjToShape(JSON.parse(userInterface.textArea.value));
-            userInterface.refreshModel();
         }
     }
     get snappedMouseCoords(): Cartesian {
